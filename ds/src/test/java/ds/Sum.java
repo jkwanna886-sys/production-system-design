ad18753f7fb44b4102bff42343bf460ee044ac88
j191 <16801784+j191@user.noreply.gitee.com>
2026-04-08 11:19:46 +0800

    sum

---
+++ ds/src/test/java/ds/Sum.java
@@ -0,0 +1,27 @@
+package ds;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class Sum {
+    @Test
+    public void test() {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.println(stream(arr));
+    }
+
+    int sumLoop(int[] arr) {
+        int sum = 0;
+
+        for (int e : arr) {
+            sum+=e;
+        }
+        return sum;
+    }
+
+    int stream(int[] arr) {
+        return Arrays.stream(arr).sum();
+    }
+}
