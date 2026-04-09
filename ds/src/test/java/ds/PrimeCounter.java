884aac8be87886b0d76c0b96fd0eaa6e22216dbb
j191 <16801784+j191@user.noreply.gitee.com>
2026-04-09 12:12:09 +0800

    prime

---
+++ ds/src/test/java/ds/PrimeCounter.java
@@ -0,0 +1,43 @@
+package ds;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+/*
+n=10
+
+2/3/5/7
+ i=2:
+     2*2=4
+     2*3=6
+     2*4=8
+     2*5=10
+ i=3(3*1 and 3*2 already handled before)
+    3*3=9
+ */
+public class PrimeCounter {
+    @Test
+    public void test() {
+        int n = 10;
+
+        Boolean[] primes = new Boolean[n+1];
+
+        Arrays.fill(primes, true);
+        primes[0] = false;
+        primes[1] = false;
+
+        for(int i=2; i*i<=n; i++) {
+            if(primes[i]) {
+                for(int j=i*i; j<=n; j+=i) {
+                    primes[j] = false;
+                }
+            }
+        }
+
+        long count = Arrays.stream(primes).filter(p -> p).count();
+
+        Assert.assertEquals(4, count);
+    }
+}
