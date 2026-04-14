320db46bf82e5fbcc8cadb93b7370781ddf7f005
j191 <16801784+j191@user.noreply.gitee.com>
2026-04-14 12:33:32 +0800

    242. Valid Anagram: balance

---
+++ ds/src/test/java/T.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class T {
+    @Test
+    public void test() {
+
+    }
+
+}
--- ds/src/test/java/ds/ValidAnagram.java
+++ ds/src/test/java/ds/ValidAnagram.java
@@ -14,13 +14,44 @@ public class ValidAnagram {
         String s = "anagram";
         String t = "nagaram";
 
+        Assert.assertEquals(true, balance(s, t));
+    }
+
+    /**
+     * convert into array
+     * sort
+     * compare
+     * time:nlog(n)
+     * space: n
+     */
+    boolean sort(String s, String t) {
         char[] charsS = s.toCharArray();
         char[] charsT = t.toCharArray();
 
         Arrays.sort(charsS);
         Arrays.sort(charsT);
 
-        boolean valid = Arrays.equals(charsS, charsT);
-        Assert.assertEquals(true, valid);
+        return Arrays.equals(charsS, charsT);
+    }
+
+    /**
+     * s increase proper index
+     * t decrease proper index
+     * check balance
+     */
+    boolean balance(String s, String t) {
+        if(s.length()!=t.length()) return false;
+
+        int[] balance = new int[26];
+        for(int i=0; i<s.length(); i++) {
+
+            balance[s.charAt(i)-'a']++;
+            balance[t.charAt(i)-'a']--;
+        }
+
+        for (int i : balance) {
+            if(i!=0) return false;
+        }
+        return true;
     }
 }
