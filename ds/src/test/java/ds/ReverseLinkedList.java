e02cb364d02386ef4540dba6ef6c89fe79665fb6
j191 <16801784+j191@user.noreply.gitee.com>
2026-04-19 11:33:57 +0800

    206. Reverse Linked List

---
+++ ds/src/test/java/ds/ReverseLinkedList.java
@@ -0,0 +1,57 @@
+package ds;
+
+import ds.dto.SingleNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 206. Reverse Linked List
+ * https://leetcode.com/problems/reverse-linked-list/
+ */
+public class ReverseLinkedList {
+    private SingleNode head;
+
+    @BeforeEach
+    public void init() {
+        head = new SingleNode(1);
+        SingleNode sec = new SingleNode(2);
+        SingleNode third = new SingleNode(3);
+        SingleNode fourth = new SingleNode(4);
+        SingleNode fifth = new SingleNode(5);
+
+        head.next = sec;
+        sec.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+    }
+
+    @Test
+    public void test() {
+        traverse(head);
+        SingleNode reverse = reverse(head);
+        System.out.println("-----");
+        traverse(reverse);
+    }
+
+    SingleNode reverse(SingleNode head) {
+        SingleNode pre = null;
+        SingleNode cur = head;
+        while (cur!=null) {
+            SingleNode tempNext = cur.next; // store the rest list
+            cur.next = pre; // backward instead of forward
+            pre = cur; // move previous
+            cur = tempNext; // go next operation
+        }
+
+        return pre;
+    }
+
+    void traverse(SingleNode head) {
+        SingleNode cur = head.next;
+        System.out.print(head.value + " ");
+        while (cur!=null) {
+            System.out.print(cur.value + " ");
+            cur = cur.next;
+        }
+    }
+}
---
+++ ds/src/test/java/ds/dto/SingleNode.java
@@ -0,0 +1,10 @@
+package ds.dto;
+
+public class SingleNode {
+    public int value;
+    public SingleNode next;
+
+    public SingleNode(int value) {
+        this.value = value;
+    }
+}
